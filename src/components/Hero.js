93a4ac47ea3c8b39d8f0c7a8d0bd320ab3d29a77
glabokta <124001355+glabokta@users.noreply.github.com>
2024-03-21 00:58:24 -0500

    description changed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,11 +5,11 @@ import logo from "../assets/logo.svg";
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+    <h1 className="mb-4">Custom Prompts Demo</h1>
 
     <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
+      This is a sample application that demonstrates custom prompts for signup. Note that
+      you will be redirected to custom domain during login, which is a basic requirement for custom prompts.</a>
     </p>
   </div>
 );
